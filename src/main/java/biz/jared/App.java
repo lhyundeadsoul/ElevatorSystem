7cc8cd6dae217310e211968b98106546852bee81
jared <yuchen.lhy@alibaba-inc.com>
2017-08-29 12:13:35 +0800

    first commit with arch pic

---
+++ src/main/java/biz/jared/App.java
@@ -0,0 +1,13 @@
+package biz.jared;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
